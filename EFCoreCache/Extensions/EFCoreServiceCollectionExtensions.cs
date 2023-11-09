de4921dd50e6f532766df2e008645d4f4804a01d
NamNguyen09 <nam.nguyenvan@orientsoftware.com>
2023-11-09 14:26:55 +0700

    Add EFCoreServiceCollectionExtensions

---
+++ EFCoreCache/Extensions/EFCoreServiceCollectionExtensions.cs
@@ -0,0 +1,80 @@
+﻿using EFCoreCache.CachePolicies;
+using EFCoreCache.Hashes;
+using EFCoreCache.Interfaces;
+using EFCoreCache.Processors;
+using EFCoreCache.Providers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace EFCoreCache.Extensions
+{
+    public static class EFCoreServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEFCoreSecondLevelCache(
+                       this IServiceCollection services,
+                       Action<EFCoreCacheOptions> options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            services.AddMemoryCache();
+            services.AddMemoryCache();
+            services.TryAddSingleton<IEFCoreDebugLogger, EFCoreDebugLogger>();
+            services.TryAddSingleton<IEFCoreCacheKeyPrefixProvider, EFCoreCacheKeyPrefixProvider>();
+            services.TryAddSingleton<IEFCoreCacheKeyProvider, EFCoreCacheKeyProvider>();
+            services.TryAddSingleton<IEFCoreCachePolicyParser, EFCoreCachePolicyParser>();
+            services.TryAddSingleton<IEFCoreSqlCommandsProcessor, EFCoreSqlCommandsProcessor>();
+            services.TryAddSingleton<IEFCoreCacheDependenciesProcessor, EFCoreCacheDependenciesProcessor>();
+            services.TryAddSingleton<ILockProvider, LockProvider>();
+            services.TryAddSingleton<IMemoryCacheChangeTokenProvider, EFMemoryCacheChangeTokenProvider>();
+            services.TryAddSingleton<IDbCommandInterceptorProcessor, DbCommandInterceptorProcessor>();
+            services.TryAddSingleton<EFCoreCacheInterceptor>();
+
+            ConfigOptions(services, options);
+
+            return services;
+        }
+
+        private static void ConfigOptions(IServiceCollection services, Action<EFCoreCacheOptions> options)
+        {
+            var cacheOptions = new EFCoreCacheOptions();
+            options.Invoke(cacheOptions);
+
+            AddHashProvider(services, cacheOptions);
+            AddCacheServiceProvider(services, cacheOptions);
+            AddOptions(services, cacheOptions);
+        }
+
+        private static void AddHashProvider(IServiceCollection services, EFCoreCacheOptions cacheOptions)
+        {
+            if (cacheOptions.Settings.HashProvider == null)
+            {
+                services.TryAddSingleton<IEFCoreHashProvider, XxHash64Unsafe>();
+            }
+            else
+            {
+                services.TryAddSingleton(typeof(IEFCoreHashProvider), cacheOptions.Settings.HashProvider);
+            }
+        }
+
+        private static void AddOptions(IServiceCollection services, EFCoreCacheOptions cacheOptions)
+        {
+            services.TryAddSingleton(Options.Create(cacheOptions.Settings));
+        }
+
+        private static void AddCacheServiceProvider(IServiceCollection services, EFCoreCacheOptions cacheOptions)
+        {
+            if (cacheOptions.Settings.CacheProvider == null)
+            {
+                services.TryAddSingleton<IEFCoreCacheServiceProvider, EFCoreMemoryCacheServiceProvider>();
+            }
+            else
+            {
+                services.TryAddSingleton(typeof(IEFCoreCacheServiceProvider), cacheOptions.Settings.CacheProvider);
+            }
+        }
+    }
+}
